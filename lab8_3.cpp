0373afce73a16e86b50715e97a0948790dacf56e
Yamami1221 <anak2547@gmail.com>
2022-12-20 16:21:38 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,7 +2,11 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x >= 'A' && x <= 'Z'){
+		return (x == 'A') ? 'Z' : x - 1;
+	} else {
+		return '0';
+	}
 }
 
 int main(){
